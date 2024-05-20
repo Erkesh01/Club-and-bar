8832bb32615b35bc211bf6864fd3ba9576d2261f
Erkesh01 <erkintur2@gmail.com>
2024-05-20 14:51:23 +0600

    main project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import Footer from "./components/footer/footer";
+import Section1 from "./components/section1/section1";
+import Section2 from "./components/section2/section2";
+import Section3 from "./components/section3/section3";
+import Header from "./tariel/header/Header";
+import Kitchen from "./tariel/kitchen/Kitchen";
+import Main from "./tariel/main_section/Main";
 
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <Main/>
+      <Kitchen/>
+      <Section1/>
+      <Section2/>
+      <Section3/>
+      <Footer/>
+
+
+      
     </div>
   );
 }
