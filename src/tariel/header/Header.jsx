08a120ac6414c795a4cb439a041f8a02c4381076
Tariel011 <tariel.muratbekov.kk@gmail.com>
2024-05-22 14:23:49 +0600

    tariel

--- src/tariel/header/Header.jsx
+++ src/tariel/header/Header.jsx
@@ -1,20 +1,27 @@
-import React from "react";
 import "./Header.css";
 import { assets } from "../assets/data";
+import { useRef } from "react";
 
 const Header = () => {
+  const menuRef = useRef();
+  const dropdown_toggle = (e) => {
+    menuRef.current.classList.toggle("nav-menu-visible");
+    e.target.classList.toggle("open");
+  };
+
   return (
     <div className="header">
       <div className="header__container">
         <img className="header__img" src={assets.icon1} />
-        <nav>
+        <img className="nav__dropdown" onClick={dropdown_toggle} src={assets.icon5} />
+        <ul ref={menuRef} className="nav__menu">
           <li>Главная</li>
           <li>Меню</li>
           <li>Афиша</li>
           <li>Сотрудничество</li>
           <li>Галерея</li>
           <li>Новости</li>
-        </nav>
+        </ul>
         <button className="header__btn">БРОНИРОВАНИЕ</button>
       </div>
     </div>
--- src/tariel/kitchen/Kitchen.jsx
+++ src/tariel/kitchen/Kitchen.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Kitchen.css";
 import "./KitchenProps.jsx";
-import { assets, product } from "../assets/data.jsx";
+import { assets, product, product2 } from "../assets/data.jsx";
 import KitchenProps from "./KitchenProps.jsx";
 
 const Kitchen = () => {
+  const [allMenu, setAllMenu] = useState([true, true]);
+  const menuClick = (index) => {
+    setAllMenu((eli) => {
+      const newAllMenu = [...eli];
+      newAllMenu[index] = !newAllMenu[index];
+      return newAllMenu;
+    });
+    // if(menuClick) {
+    //   index.target.classList.remove('arrow')
+    // }
+  };
   return (
     <div className="kitchen">
       <button className="btn__top__menu">Top “Manhattan” menu</button>
       <h1>Кухня</h1>
       <div className="kitchen__container">
-        <button className="arrow"><img src={assets.icon2}/></button>
+        <button className="arrow">
+          <img src={assets.icon2} />
+        </button>
         <div className="food__block">
           {product.map((e) => {
             return (
@@ -23,9 +36,26 @@ const Kitchen = () => {
             );
           })}
         </div>
-        <button className="arrow"><img src={assets.icon3}/></button>
+        <button className="arrow">
+          <img src={assets.icon3} />
+        </button>
       </div>
-      <button className="btn__menu">все меню</button>
+      <div className="food__block2">
+        {allMenu[0] &&
+          product2.map((e) => {
+            return (
+              <KitchenProps
+                img={e.img}
+                price={e.price}
+                weight={e.weight}
+                desc={e.desc}
+              />
+            );
+          })}
+      </div>
+      <button onClick={() => menuClick(0)} className="btn__menu" style={{marginTop: '-300px'}}>
+        все меню
+      </button>
     </div>
   );
 };
