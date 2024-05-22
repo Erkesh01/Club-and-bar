1ddea0a652d48f28f6d8c32c81d4e051d535429c
Tariel011 <tariel.muratbekov.kk@gmail.com>
2024-05-22 15:15:56 +0600

    tariel

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from "./tariel/header/Header";
 import Sections from "./Sections/Sections";
 import Footer from "./components/footer/footer";
 import Corparatif from "./edil/corparatif/corparatif";
+import AfishaMain from "./edil/afisha/afishMain";
+import Gallery from "./edil/gallery/gallery";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Sections />} />
         <Route path="/Сотрудничества" element={<Corparatif/>}/>
+        <Route path="/Афиша" element={<AfishaMain/>}/>
+        <Route path="/Галерея" element={<Gallery/>}/>
       </Routes>
       <Footer/>
     </div>
